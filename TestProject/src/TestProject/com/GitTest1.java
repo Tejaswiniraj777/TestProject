470c256cfe86ecf2005c294217c07cac666409d6
Dell <Dell@LAP-W-PAVAGADASITE2>
2022-07-13 23:18:17 +0530

    First Commit

--- TestProject/src/TestProject/com/GitTest1.java
+++ TestProject/src/TestProject/com/GitTest1.java
@@ -1,11 +1,11 @@
-
+package TestProject.com;
 public class GitTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
 		int b=20;
-		//do
+		//d
 
 	}
 
